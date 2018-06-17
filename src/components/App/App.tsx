30223007882393133a7ae7f8b8d8697dd5a5d024
Kévin LEMELE <lemele.kevin@gmail.com>
2018-06-17 21:02:35 +0200

    Component - AppBar
     - Added components AppBar
     - Set redirection to survey

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,11 @@
-import * as React from 'react';
-
+import * as React from "react";
+import { AppBar } from "../AppBar";
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-        <header>
-          <h1>Welcome to React</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <AppBar />
       </div>
     );
   }
---
+++ src/components/AppBar/AppBar.test.tsx
@@ -0,0 +1,20 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+import AppBar from "./AppBar";
+
+describe("AppBar component", () => {
+  let minProps: object;
+
+  beforeEach(() => {
+    minProps = {
+      classes: {},
+      t: (sentence: string) => sentence
+    };
+  });
+
+  it("renders correctly", () => {
+    const wrapper = shallow(<AppBar {...minProps} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
