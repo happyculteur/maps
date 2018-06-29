b9ae67a0228a187ea6715553dfa7f598826e61a4
Kévin LEMELE <lemele.kevin@gmail.com>
2018-06-29 17:41:55 +0200

    Wip grid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { AppBar } from "../AppBar";
+import { Grid } from "../Grid";
 
 class App extends React.Component {
   public render() {
+    const data = require("../../configuration/data/fakedata.json");
+
     return (
       <div>
         <AppBar />
+        <Grid data={data} />
       </div>
     );
   }
