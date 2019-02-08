74b60af8924600e18024cff7b0dc7b68b0d0ac36
Kévin LEMELE <lemele.kevin@gmail.com>
2019-02-08 22:21:23 +0100

    Added - Component: Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { makeStyles } from "@material-ui/styles";
+import React from "react";
+
+const useStyles = makeStyles(theme => ({
+  Footer: {
+    borderRadius: "2px",
+    boxShadow: `
+    -1px -2px 4px -1px rgba(0,0,0,0.2),
+    -1px -4px 5px -1px rgba(0,0,0,0.14),
+    -1px -1px 10px -1px rgba(0,0,0,0.12)
+    `,
+    gridArea: "Footer",
+    marginTop: "2px",
+    zIndex: 1
+  },
+  Link: {
+    "&:hover": {
+      color: theme.palette.secondary.dark
+    },
+    color: theme.palette.primary.dark,
+    textDecoration: "none"
+  },
+  Social: {
+    alignContent: "center",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "space-around",
+    marginTop: "8px"
+  }
+}));
+
+const Footer: React.FunctionComponent = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.Footer}>
+      <div className={classes.Social}>
+        <a
+          className={classes.Link}
+          href="https://www.facebook.com/happyculteur.co/"
+        >
+          <FontAwesomeIcon icon={["fab", "facebook"]} size="3x" />
+        </a>
+        <a className={classes.Link} href="https://twitter.com/happyculteur_">
+          <FontAwesomeIcon icon={["fab", "twitter"]} size="3x" />
+        </a>
+        <a
+          className={classes.Link}
+          href="https://www.instagram.com/happyculteur.co/"
+        >
+          <FontAwesomeIcon icon={["fab", "instagram"]} size="3x" />
+        </a>
+        <a className={classes.Link} href="mailto:hello@happyculteur.co">
+          <FontAwesomeIcon icon="envelope" size="3x" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
