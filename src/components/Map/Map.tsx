6bedff9e92628ad592425ac0cb0dfa0893b9994c
Kévin LEMELE <lemele.kevin@gmail.com>
2019-02-11 22:21:23 +0100

    Added - Component: Map

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,60 @@
+import { makeStyles } from "@material-ui/styles";
+import { RouteComponentProps } from "@reach/router";
+import { GeoJsonObject } from "geojson";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import React from "react";
+import { toast } from "react-toastify";
+import boundaries from "./boundaries.json";
+
+const useStyles = makeStyles({
+  Map: {
+    gridArea: "Map",
+    height: "100%",
+    padding: "6px 0"
+  },
+  mapContainer: {
+    height: "100%"
+  }
+});
+
+const Map: React.FunctionComponent<RouteComponentProps> = props => {
+  const classes = useStyles();
+  const drawMap = async () => {
+    try {
+      const osmURL = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
+      const attribution =
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+
+      // Create map
+      const map = L.map("mymap", {
+        center: [48.858, 2.351499],
+        layers: [
+          L.tileLayer(osmURL, {
+            attribution
+          })
+        ],
+        zoom: 13
+      });
+
+      // Add GeoJson boundaries
+      L.geoJSON(boundaries as GeoJsonObject).addTo(map);
+      // Bound map to defined area
+      map.setMaxBounds(map.getBounds());
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  React.useEffect(() => {
+    drawMap();
+  }, []);
+
+  return (
+    <div className={classes.Map}>
+      <div id="mymap" className={classes.mapContainer} />
+    </div>
+  );
+};
+
+export default Map;
