de9bfee9835db8d6a1e1fe94025d6e95cb14802a
Kévin LEMELE <lemele.kevin@gmail.com>
2019-02-16 22:21:23 +0100

    Added - Component: Router

---
+++ src/Router/Router.tsx
@@ -0,0 +1,52 @@
+import { makeStyles } from "@material-ui/styles";
+import { Router as ReachRouter } from "@reach/router";
+import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { CardList } from "../components/CardList";
+import { Map } from "../components/Map";
+import { MapAndCardList } from "../pages/MapAndCardList";
+
+const useStyles = makeStyles({
+  Router: {
+    "@global": {
+      "::-webkit-scrollbar": {
+        backgroundColor: "#F5F5F5",
+        width: "10px"
+      },
+      "::-webkit-scrollbar-thumb": {
+        backgroundColor: "#F90",
+        backgroundImage: `-webkit-linear-gradient(
+        90deg,
+        rgba(255, 255, 255, .2) 25%,
+        transparent 25%,
+        transparent 50%,
+        rgba(255, 255, 255, .2) 50%,
+        rgba(255, 255, 255, .2) 75%,
+        transparent 75%,
+        transparent
+      )`
+      },
+      "::-webkit-scrollbar-track": {
+        "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.3)",
+        backgroundColor: "#F5F5F5"
+      }
+    },
+    gridArea: "Router",
+    height: "100%",
+    zIndex: 0
+  }
+});
+
+const Router: React.FunctionComponent = () => {
+  const classes = useStyles();
+
+  return (
+    <ReachRouter className={classes.Router}>
+      <Map path="/map" default={true} />
+      <CardList path="/list" />
+      <MapAndCardList default={true} path="/" />
+    </ReachRouter>
+  );
+};
+
+export default Router;
