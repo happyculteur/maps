b13cb38e52d4596c85067af9e8fc9c4a2b021e06
Kévin LEMELE <lemele.kevin@gmail.com>
2019-02-20 22:21:23 +0100

    Update - Configuration: sentry
    - code style

--- src/configuration/sentry/sentry.ts
+++ src/configuration/sentry/sentry.ts
@@ -1,21 +1,19 @@
-import * as Raven from 'raven-js';
+import Raven from "raven-js";
 
 const sentry = () => {
   const configurations = {
     key: process.env.SENTRY_KEY,
-    project: process.env.SENTRY_PROJECT 
-  }
+    project: process.env.SENTRY_PROJECT
+  };
 
   if (process.env.SENTRY_KEY && process.env.SENTRY_PROJECT) {
-    Raven
-      .config(
-        `https://${configurations.key}@sentry.io/${configurations.project}`,
-        {
-          debug: process.env.NODE_ENV !== 'production',
-          environment: process.env.NODE_ENV
-        }
-      )
-      .install();
+    Raven.config(
+      `https://${configurations.key}@sentry.io/${configurations.project}`,
+      {
+        debug: process.env.NODE_ENV !== "production",
+        environment: process.env.NODE_ENV
+      }
+    ).install();
   }
-}
+};
 export default sentry;
