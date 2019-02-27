7acc9fb6bc7ed0a21dc7ac67db049ad892fe2ddb
Kévin LEMELE <lemele.kevin@gmail.com>
2019-02-27 22:21:23 +0100

    Update - component: AppBar
    - Remove old test link for component visualisation
    - Remove css in favor of MaterialUi theme usage

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,12 +1,4 @@
-import {
-  AppBar as MuiAppBar,
-  Button,
-  IconButton,
-  Toolbar
-} from "@material-ui/core";
-import GridOn from "@material-ui/icons/GridOn";
-import Map from "@material-ui/icons/Map";
-import VerticalSplit from "@material-ui/icons/VerticalSplit";
+import { AppBar as MuiAppBar, Button, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { Link } from "@reach/router";
 import React from "react";
@@ -23,24 +15,6 @@ export const useStyles = makeStyles(theme => ({
     height: "60px",
     width: "auto"
   },
-  SurveyButton: {
-    "&:hover": {
-      backgroundColor: theme.palette.secondary.dark
-    },
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.primary.light,
-    marginLeft: "auto"
-  },
-  button: {
-    "&:hover": {
-      color: theme.palette.secondary.dark
-    },
-    color: theme.palette.primary.light
-  },
-  buttons: {
-    borderLeft: "solid black 2px",
-    marginLeft: "2%"
-  },
   empty: {
     flexGrow: 1
   }
@@ -54,27 +28,13 @@ export const AppBar: React.FunctionComponent = props => {
     <div className={classes.AppBar}>
       <MuiAppBar position="static" color="default">
         <Toolbar>
-          <img className={classes.Logo} src={logo} alt="Logo" />
-          <div className={classes.buttons}>
-            <Link to="/map">
-              <IconButton className={classes.button} aria-label="Menu">
-                <Map />
-              </IconButton>
-            </Link>
-            <Link to="/list">
-              <IconButton className={classes.button} aria-label="Menu">
-                <GridOn />
-              </IconButton>
-            </Link>
-            <Link to="/">
-              <IconButton className={classes.button} aria-label="Menu">
-                <VerticalSplit />
-              </IconButton>
-            </Link>
-          </div>
+          <Link to="/">
+            <img className={classes.Logo} src={logo} alt="Logo" />
+          </Link>
           <div className={classes.empty} />
           <Button
-            className={classes.SurveyButton}
+            variant="contained"
+            color="secondary"
             href={process.env.REACT_APP_HAPPYCULTEUR_SURVEY}
             data-e2e="SurveyButton"
           >
