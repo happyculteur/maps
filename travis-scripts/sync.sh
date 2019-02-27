156c06f4ef12f95b8a2190b69db49f3223ed5e29
RiceBowlJr <alexandre.kervadec@gmail.com>
2019-02-27 12:23:59 +0100

    fix: typo in if condition blocking the CI CD

--- travis-scripts/sync.sh
+++ travis-scripts/sync.sh
@@ -5,7 +5,7 @@ ENV=$1
 npm install --only=prod
 npm run build
 
-if [ $ENV = "production" ] then
+if [ $ENV = "production" ]; then
   aws s3 sync ./build/ s3://app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
 else
   aws s3 sync ./build/ s3://$ENV.app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
