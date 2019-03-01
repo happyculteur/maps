2c1f758da8eef418ca384f9cdf62989552b17096
RiceBowlJr <alexandre.kervadec@gmail.com>
2019-03-01 13:40:19 +0100

    fix: CI deploying in the wrong bucket

--- travis-scripts/sync.sh
+++ travis-scripts/sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ENV=$1
+BRANCH=$1
 
 npm install --only=prod
 npm run build
 
-if [ $ENV = "production" ]; then
+if [ $BRANCH = "master" ]; then
   aws s3 sync ./build/ s3://app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
 else
   aws s3 sync ./build/ s3://$ENV.app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
