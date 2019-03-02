361dfc4eafc62637d0592dc36efbb4a4730ecd8c
Kévin LEMELE <lemele.kevin@gmail.com>
2019-03-02 22:21:23 +0100

    Update - component: CardList
    - Move Data aggregator function to dedicated file
    - Change use of type to be more coherent

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -4,11 +4,11 @@ import classnames from "classnames";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import { SyncLoader } from "react-spinners";
-import uuid from "uuid";
+import { IUser } from "../../types";
+import { dataSourceTransformer } from "../../utils";
 import { Card } from "../Card";
 import { InfiniteScroll } from "../InfiniteScroll";
 import data from "./fakedata.json";
-import { IBeekeeper, IInformation, ISpace, ITraining, IUser } from "./types";
 
 const useStyles = makeStyles({
   CardList: {
@@ -39,52 +39,8 @@ const CardList: React.FunctionComponent<
     await new Promise(resolve => setTimeout(resolve, 3000));
 
     return data.reduce(
-      (accumulator, current) => {
-        const information: IInformation = {
-          category: current.category as IInformation["category"],
-          interrest:
-            current["individual-insterest-in-happyculteur"] ||
-            current["beekeeper-interest-in-happyculteur"],
-          level: current["individual-level"] || current["beekeeper-level"],
-          location: current.address as [number, number],
-          name: current.name,
-          uuid: uuid()
-        };
-        const space: ISpace | undefined = current["individual-space-has-space"]
-          ? {
-              description: current["individual-space-description"],
-              location: current["individual-space-location"] as [
-                number,
-                number
-              ],
-              size: current["individual-space-size"],
-              type: current["individual-space-type"] as ISpace["type"]
-            }
-          : undefined;
-        const beekeeper: IBeekeeper | undefined =
-          information.category === "Apiculteur"
-            ? {
-                installation: current["beekeeper-installation-type-wished"],
-                nest: current["beekeeper-nest-number"],
-                since: current["beekeeper-time"]
-              }
-            : undefined;
-        const training: ITraining | undefined =
-          information.category === "Apiculteur"
-            ? {
-                isBilled: current["beekeeper-is-formation-billed"],
-                location: (current["beekeeper-formation-place"] ||
-                  information.location) as [number, number],
-                target: current["beekeeper-formation-audience"]
-              }
-            : undefined;
-
-        accumulator.push({
-          beekeeper,
-          information,
-          space,
-          training
-        });
+      (accumulator, userInfo) => {
+        accumulator.push(dataSourceTransformer(userInfo));
 
         return accumulator;
       },
@@ -99,7 +55,7 @@ const CardList: React.FunctionComponent<
       className={classnames(classes.CardList, props.className)}
       loaderElement={<SyncLoader />}
     >
-      {(user: IUser) => <Card key={user.information.uuid} {...user} />}
+      {(user: IUser) => <Card key={user.information.uuid} user={user} />}
     </InfiniteScroll>
   );
 };
