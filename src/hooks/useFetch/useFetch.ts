ef4bc687786f96f6ee7d0df2a08bab00629651cd
Kévin LEMELE <lemele.kevin@gmail.com>
2019-03-30 22:21:23 +0100

    Added - hook: useFetch
    - Added new hook useFetch for fetching async data

---
+++ src/hooks/useFetch/useFetch.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export enum typeReturned {
+  JSON = "JSON",
+  TEXT = "TEXT"
+}
+
+function useFetch(
+  url: string,
+  valueToWatch: any,
+  returnType: typeReturned
+): string {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then(response =>
+        returnType === typeReturned.TEXT ? response.text() : response.json()
+      )
+      .then(responseData => setData(responseData));
+  }, [valueToWatch]);
+
+  return data;
+}
+
+export default useFetch;
