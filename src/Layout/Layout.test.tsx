56938d9bccfe55193b1bb0c250d3e74d71660db3
Kévin LEMELE <lemele.kevin@gmail.com>
2019-04-10 16:12:32 +0200

    Deleted - Test: Old test
    - Remove old test from Layout and AppBar
    - Shall be done properly with codebase up to date
    - Only left one e2e and unit test to prevent build failure

--- src/Layout/Layout.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import React from "react";
-import App from "./App";
-
-it("should renders without crashing", () => {
-  const wrapper = shallow(<App />);
-
-  expect(wrapper.debug()).toMatchSnapshot();
-});
