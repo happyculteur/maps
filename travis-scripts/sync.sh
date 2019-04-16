3e92511bcd3c227864db45ce757729043fa2e821
RiceBowlJr <alexandre.kervadec@gmail.com>
2019-04-16 19:56:25 +0200

    fix(travis): wrong condition on branch

--- travis-scripts/sync.sh
+++ travis-scripts/sync.sh
@@ -8,5 +8,5 @@ npm run build
 if [ $BRANCH = "master" ]; then
   aws s3 sync ./build/ s3://app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
 else
-  aws s3 sync ./build/ s3://$ENV.app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
+  aws s3 sync ./build/ s3://$BRANCH.app.happyculteur.co --exclude ".git" --exclude ".travis.yml" --cache-control max-age=${MAX_AGE} --delete
 fi
